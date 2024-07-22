931fc6a2fba012173f2e529790ca29f346398107
Varun Agarwal <varunagarwal10.2@gmail.com>
2024-07-22 15:39:16 +0530

    added response body

--- routes/routes.py
+++ routes/routes.py
@@ -11,13 +11,17 @@ class ContinueChat(BaseModel) :
     chatId: str
     user_response: str
 
+class ApiResponse(BaseModel):
+    workFlowChatId: str
+    question: str
+
 router = APIRouter(prefix="/workflowchat", tags=["workflow_chat"])
 
-@router.post("/trigger/{workflowid}", response_description="trigger a workflow chat and return greet message along with chat Id", status_code=status.HTTP_201_CREATED)  
+@router.post("/trigger/{workflowid}", response_description="trigger a workflow chat and return greet message along with chat Id", status_code=status.HTTP_201_CREATED, response_model=ApiResponse)  
 def trigger(request: Request, workflowid: str, session: SessionContainer = Depends(verify_session())):
     return trigger_workflow_chat(request, workflowid)
 
-@router.post("/continuechat", response_description="will return the chat Id and the next question", status_code=status.HTTP_200_OK)
+@router.post("/continuechat", response_description="will return the chat Id and the next question", status_code=status.HTTP_200_OK, response_model=ApiResponse)
 def continue_chat(request: Request, resp_body: ContinueChat, session: SessionContainer = Depends(verify_session())):
     chatid = resp_body.chatId
     user_response = resp_body.user_response
